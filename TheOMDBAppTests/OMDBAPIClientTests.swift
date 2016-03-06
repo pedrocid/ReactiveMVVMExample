50504e3efca7612e237541d38454c2c59d1d46e5
Pedro <pcid@hootap.com>
2016-03-06 16:51:54 +0100

    OMDBAPIClient tests

--- TheOMDBAppTests/OMDBAPIClientTests.swift
+++ TheOMDBAppTests/OMDBAPIClientTests.swift
@@ -15,4 +15,55 @@ import Nimble
 class OMDBAPIClientTests: QuickSpec {
     
     
+    override func spec(){
+    
+        describe("test if client returns a Movie"){
+        
+            context("returns a movie from local json"){
+            
+                it("returns a movie"){
+                
+            
+                    let networking = StubNetworking()
+                    let apiClient: APIClient = OMDBAPIClient(network: networking)
+                    var response: Movie?
+                    
+                    apiClient.getMovieWithTitle("Matrix").on(next: { response = $0 }).start()
+                    
+                    expect(response).toEventuallyNot(beNil(),timeout: 5)
+                    
+                }
+            }
+            
+        }
+    
+        
+    }
+    
+    
+}
+
+
+private class StubNetworking: Networking {
+    
+    func requestJSON(url: String, parameters: [String : AnyObject]?)
+        -> SignalProducer<AnyObject, NSError>{
+    
+            return SignalProducer { observer, disposable in
+            
+                observer.sendNext(getMovieJSON())
+                observer.sendCompleted()
+                
+            }
+    }
+}
+
+
+private func getMovieJSON() -> [String:AnyObject]{
+
+    return ["Title":"Matrix",
+            "Director":"Watchowswki",
+            "Year":"1998",
+            "Plot":"Wowow"]
+    
 }
